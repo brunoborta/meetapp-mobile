ce14f8dab9d0251d5d5ff6f4cf9cf84119928562
Bruno "Borta" Bortagaray <bruno.bortagaray@gmail.com>
2019-08-27 16:57:15 -0300

    Adicionado o yup

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,24 +1,81 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { Image, Text } from 'react-native';
+import PropTypes from 'prop-types';
+import { Formik } from 'formik';
+import * as yup from 'yup';
 
 import Background from '~/components/Background';
-import Button from '~/components/Button';
-import Input from '~/components/Input';
 
 import logo from '~/assets/logo.png';
 
-import { Container, Form } from './styles';
+import {
+  Container,
+  Form,
+  Anchor,
+  AnchorText,
+  FormInput,
+  SubmitButton,
+} from './styles';
 
-export default function SignIn() {
+function SignIn({ navigation }) {
+  const passwordRef = useRef();
+
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  const schema = yup.object().shape({
+    email: yup
+      .string()
+      .email('Digite um e-mail válido')
+      .required('Preencha o campo de e-mail'),
+    password: yup
+      .string()
+      .min(6, 'A senha deve ter no mínimo 6 caracteres')
+      .required('Preencha o campo de senha'),
+  });
+
+  function submit() {}
   return (
     <Background>
       <Container>
         <Image source={logo} />
         <Form>
-          <Input placeholder="Digite seu e-mail" />
-          <Button>Entrar</Button>
+          <FormInput
+            placeholder="Digite seu e-mail"
+            autoCapitalize="none"
+            autoCorrect="false"
+            autoFocus="true"
+            keyboardType="email-address"
+            onSubmitEditing={() => passwordRef.current.focus()}
+            returnKeyType="next"
+            onChange={setEmail}
+            value={email}
+          />
+          <FormInput
+            placeholder="Sua senha secreta"
+            autoCapitalize="none"
+            autoCorrect="false"
+            secureTextEntry
+            ref={passwordRef}
+            onSubmitEditing={submit}
+            onChange={setPassword}
+            value={password}
+            returnKeyType="send"
+          />
+          <SubmitButton onPress={submit}>Entrar</SubmitButton>
         </Form>
+        <Anchor onPress={() => navigation.navigate('SignUp')}>
+          <AnchorText>Criar conta grátis</AnchorText>
+        </Anchor>
       </Container>
     </Background>
   );
 }
+
+SignIn.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
+};
+
+export default SignIn;
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,6 +1,9 @@
 import { Platform } from 'react-native';
 import styled from 'styled-components/native';
 
+import Button from '~/components/Button';
+import Input from '~/components/Input';
+
 export const Container = styled.KeyboardAvoidingView.attrs({
   enabled: Platform.OS === 'ios',
   behavior: 'padding',
@@ -15,4 +18,22 @@ export const Form = styled.View`
   margin-top: 50px;
   align-self: stretch;
   justify-content: center;
+`;
+
+export const Anchor = styled.TouchableOpacity`
+  margin-top: 20px;
+  align-self: center;
+`;
+export const AnchorText = styled.Text`
+  color: rgba(255, 255, 255, 0.6);
+  font-weight: bold;
+  font-size: 16px;
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-top: 5px;
+`;
+
+export const FormInput = styled(Input)`
+  margin-bottom: 10px;
 `;
